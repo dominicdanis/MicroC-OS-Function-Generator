ed0fadb4dd97e40547f2e071078e33076277813a
danisd <danisd@wwu.edu>
2022-02-17 13:25:09 -0800

    Initial public functions implemented. May change to decouple MemoryTools from AppLab3

--- group8Lab3Project/source/MemoryTools.c
+++ group8Lab3Project/source/MemoryTools.c
@@ -1,12 +1,15 @@
 /*******************************************************************************
- * EECE344 Lab 2 Code
- *   MemoryTools.c is a module with a public function used to return the checksum
- *   of 2 memory addresses
- *
- * Nick Coyle, 10/11/2021
- *******************************************************************************/
+* EECE444 Lab 3 Code
+*   MemoryTools.c is a module containing memory interface and validation. It contains
+*   a public function for checksum and an interface for EEPROM via SPI - configured
+*   for function generator use
+*
+* 01/25/2022 Nick Coyle, Aili Emory, Dominic Danis
+*******************************************************************************/
 #include "MCUType.h"               /* Include header files                    */
 #include "MemoryTools.h"
+
+static INT8U validateConfig(SAVED_CONFIG);
 
 /*******************************************************************************
  *   MemChkSum() Given 2 addresses this calculates and returns a checksum
@@ -22,6 +25,44 @@ INT16U MemChkSum(INT8U *startaddr, INT8U *endaddr) {
         start++;
     }
     chk_sum += (INT16U)*start;            /* get the last byte outside the loop to prevent terminal count bug */
-
     return chk_sum;
 }
+/*******************************************************************************
+*   MemLoad() will load the current configurations, validate them and return
+*   either the valid configuration or the default configuration
+*******************************************************************************/
+SAVED_CONFIG MemLoad(void){
+    SAVED_CONFIG current;
+    //current = some function to read from SPI
+    if(validateConfig(current)==0){
+        current.state = SINEWAVE;
+        current.freq = 1000;
+        current.level = 10;
+    }
+    else{}
+    return current;
+}
+/*******************************************************************************
+*   SaveConfig() will save the passed configurations to EEPROM via SPI
+*******************************************************************************/
+void SaveConfig(SAVED_CONFIG current){
+    //do something here - with SPI to save current to EERPOM
+}
+/*******************************************************************************
+*   validateConfig() will validate a current configuration given Lab 3 requirements
+*   and return a boolean result
+*******************************************************************************/
+static INT8U validateConfig(SAVED_CONFIG current){
+    INT8U valid = 1;
+    if(current.level>20){
+        valid = 0;
+    }
+    else{}
+    if(current.freq>10000 || current.freq<1){
+        valid = 0;
+    }
+    else{}
+    return valid;
+}
+
+
--- group8Lab3Project/source/MemoryTools.h
+++ group8Lab3Project/source/MemoryTools.h
@@ -1,14 +1,37 @@
 /*******************************************************************************
-* EECE344 Lab 2 Code
-*   MemoryTools.c is a module with a public function used to return the checksum
-*   of 2 memory addresses
+* EECE444 Lab 3 Code
+*   MemoryTools.c is a module containing memory interface and validation. It contains
+*   a public function for checksum and an interface for EEPROM via SPI - configured
+*   for function generator use
 *
-* Nick Coyle, 10/11/2021
+* 01/25/2022 Nick Coyle, Aili Emory, Dominic Danis
 *******************************************************************************/
 
 #ifndef MEMORYTOOLS_H_
 #define MEMORYTOOLS_H_
-
+/*******************************************************************************
+*   Enumerated type for UI states. Held in MemoryTools publically because both
+*   AppLab3 and MemoryTools will need to use.
+*******************************************************************************/
+typedef enum {DEFAULT, SINEWAVE, PULSE_TRAIN} UI_STATES_T;
+/*******************************************************************************
+*   Structure for state, frequency and level. Held in MemoryTools publically
+*   because both AppLab3 and MemoryTools will need to use.
+*******************************************************************************/
+typedef struct{
+    UI_STATES_T state;
+    INT16U freq;
+    INT8U level;
+} SAVED_CONFIG;
+/*******************************************************************************
+*   MemLoad() will load the current configurations, validate them and return
+*   either the valid configuration or the default configuration
+*******************************************************************************/
+SAVED_CONFIG MemLoad(void);
+/*******************************************************************************
+*   SaveConfig() will save the passed configurations to EEPROM via SPI
+*******************************************************************************/
+void SaveConfig(SAVED_CONFIG);
 /*******************************************************************************
 *   MemChkSum() Given 2 addresses this calculates and returns a checksum
 *   checksum is defined as the 16-bit sum of each byte in the block of memory
